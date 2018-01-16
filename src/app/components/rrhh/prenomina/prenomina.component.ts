7d99ed19af103e2aaf9b23e635c4da5fdc5f1c7d
geosh2000 <albert.sanchez@pricetravel.com.mx>
2018-01-16 14:48:56 -0500

    Modulo de Prenomina v2.0

--- src/app/components/rrhh/prenomina/prenomina.component.ts
+++ src/app/components/rrhh/prenomina/prenomina.component.ts
@@ -6,6 +6,9 @@ import { ApiService } from '../../../services/api.service';
 import { InitService } from '../../../services/init.service';
 import { TokenCheckService } from '../../../services/token-check.service';
 
+import { saveAs } from 'file-saver';
+import { utils, write, WorkBook } from 'xlsx';
+
 declare var jQuery:any;
 import * as moment from 'moment-timezone';
 
@@ -23,13 +26,19 @@ export class PrenominaComponent implements OnInit {
 
   loading:Object = {}
 
+  listCortes:any
+  listCortesFlag:boolean = false
   dataSchedules:any
   dataLogs:any
   dataAusentismos:any
+  dataCxc:any
+  prenomCxc:any
   dataAsesores:any
   dataShow:any
   dataPrenom:any
   built:any
+
+  tableHeaders:any = []
 
   constructor(public _api: ApiService,
                 private _init:InitService,
@@ -52,10 +61,42 @@ export class PrenominaComponent implements OnInit {
           }
         })
 
+    this.tableHeaders = [
+      { t: 'Descansos', code: 'D', type: 'd' },
+      { t: 'Asistencias', code: 'A', type: 'd' },
+      { t: 'Capacitación', code: 'CA', type: 'd' },
+      { t: 'D Faltas JUS', code: 'FA', type: 'd' },
+      { t: 'F Faltas JUS', code: 'FA', type: 'f' },
+      { t: 'D Faltas IN', code: 'FJ', type: 'd' },
+      { t: 'F Faltas IN', code: 'FJ', type: 'f' },
+      { t: 'D Suspension', code: 'SUS', type: 'd' },
+      { t: 'F Suspension', code: 'SUS', type: 'f' },
+      { t: 'D Maternidad', code: 'INC_MT', type: 'd' },
+      { t: 'F Maternidad', code: 'INC_MT', type: 'f' },
+      { t: 'D Enfermedad', code: 'INC_EN', type: 'd' },
+      { t: 'F Enfermedad', code: 'INC_EN', type: 'f' },
+      { t: 'D Accidente', code: 'INC_AC', type: 'd' },
+      { t: 'F Accidente', code: 'INC_AC', type: 'f' },
+      { t: 'D Acc por Riesgo', code: 'INC_RT', type: 'd' },
+      { t: 'F Acc por Riesgo', code: 'INC_RT', type: 'f' },
+      { t: 'D Permiso sin g', code: 'PS', type: 'd' },
+      { t: 'F Permiso sin g', code: 'PS', type: 'f' },
+      { t: 'D Permiso con g', code: 'PC', type: 'd' },
+      { t: 'F Permiso con g', code: 'PC', type: 'f' },
+      { t: 'D Vacaciones', code: 'VAC', type: 'd' },
+      { t: 'F Vacaciones', code: 'VAC', type: 'f' },
+      { t: 'D Descanso Trabajado', code: 'DT', type: 'd' },
+      { t: 'F Descanso Trabajado', code: 'DT', type: 'f' },
+      { t: 'D Día Festivo', code: 'FES', type: 'd' },
+      { t: 'F Día Festivo', code: 'FES', type: 'f' },
+      { t: 'D Domingos Trabajados', code: 'DOM', type: 'd' },
+      { t: 'F Domingos Trabajados', code: 'DOM', type: 'f' }
+    ]
+
   }
 
   ngOnInit() {
-    this.getSchedules(25)
+    this.getCortes()
   }
 
   getSchedules( nominaId ){
@@ -139,26 +180,72 @@ export class PrenominaComponent implements OnInit {
 
                 this.loading['ausentismos'] = false
 
-                let ausent = {}
+                this.dataAusentismos = res.data
 
-                for( let aus of res.data ){
-                  if( ausent[aus.asesor] ){
-                    ausent[aus.asesor].push( aus )
-                  }else{
-                    ausent[aus.asesor] = [ aus ]
-                  }
-                }
+                this.getCxc(nominaId)
 
-                this.dataAusentismos = ausent
-
-                this.buildData()
-
-                console.log(res.data)
 
               }, err => {
                 console.log("ERROR", err)
 
                 this.loading['ausentismos'] = false
+
+                let error = err.json()
+                this.toastr.error( error.msg, `Error ${err.status} - ${err.statusText}` )
+                console.error(err.statusText, error.msg)
+
+              })
+  }
+
+  getCxc( nominaId ){
+    this.loading['cxc'] = true
+    this.prenomCxc = {}
+
+    this._api.restfulGet( nominaId, 'Prenomina/cxc')
+              .subscribe( res => {
+
+                this.loading['cxc'] = false
+                this.loading['building'] = true
+
+                this.dataCxc = res.data
+
+                for( let asesor in res.data ){
+                  for( let cxc of res.data[asesor] ){
+                    if( this.prenomCxc[asesor] ){
+                      this.prenomCxc[asesor][cxc.tipo]['monto'] += parseFloat(cxc.monto)
+                      this.prenomCxc[asesor][cxc.tipo]['detail'] = `${this.prenomCxc[asesor][cxc.tipo]['detail']}, ${cxc.localizador} (${cxc.n_pago}/${cxc.pagos})`
+                    }else{
+
+                      let other
+
+                      if( cxc.tipo == 1 ){
+                        other = 0
+                      }else{
+                        other = 1
+                      }
+
+                      this.prenomCxc[asesor] = {
+                        [cxc.tipo]: {
+                            monto: parseFloat(cxc.monto),
+                            detail: `${cxc.localizador} (${cxc.n_pago}/${cxc.pagos})`
+                          },
+                        [other]: {
+                          monto: 0,
+                          detail: ''
+                        }
+
+                      }
+                    }
+                  }
+                }
+
+                this.buildData()
+
+
+              }, err => {
+                console.log("ERROR", err)
+
+                this.loading['cxc'] = false
 
                 let error = err.json()
                 this.toastr.error( error.msg, `Error ${err.status} - ${err.statusText}` )
@@ -212,31 +299,97 @@ export class PrenominaComponent implements OnInit {
       }
     }
 
-    console.log(this.dataSchedules)
-
     let dataShow = {}
     let dataPrenom = {}
+    let tmp = ''
+    let code = ''
 
     for( let item of this.dataSchedules ){
       item['exception'] = this.getExcept( item, 'code' )
+      item['rcode'] = this.getExcept( item, 'rcode' )
       item['exc_desc'] = this.getExcept( item, 'desc' )
       item['hx'] = this.checkLengthHX( item )
+
 
       if( dataPrenom[item.asesor] ){
 
         dataPrenom[item.asesor]['hx'] += item['hx']
 
-        if( dataPrenom[item.asesor][item['exception']] ){
-          dataPrenom[item.asesor][item['exception']]++
+        if( dataPrenom[item.asesor][item.rcode] ){
+
+          if( tmp != item.rcode ){
+            tmp = item.rcode
+            code = item.rcode
+
+            if( code != item.exception){
+              if( dataPrenom[item.asesor][item['exception']] ){
+                dataPrenom[item.asesor][item['exception']]['days']++
+              }else{
+                dataPrenom[item.asesor][item['exception']] = { days: 1, dates: [{ inicio: item.Fecha, fin: item.Fecha }] }
+              }
+            }else{
+              dataPrenom[item.asesor][item.rcode].dates.push({ inicio: item.Fecha, fin: item.Fecha })
+              dataPrenom[item.asesor][code].days++
+            }
+
+
+
+          }else{
+
+            let index = dataPrenom[item.asesor][code].dates.length -1
+
+            if( code != item.exception){
+              if( dataPrenom[item.asesor][item['exception']] ){
+                dataPrenom[item.asesor][item['exception']]['days']++
+              }else{
+                dataPrenom[item.asesor][item['exception']] = { days: 1, dates: [{ inicio: item.Fecha, fin: item.Fecha }] }
+              }
+            }else{
+              dataPrenom[item.asesor][code].dates[index].fin = item.Fecha
+              dataPrenom[item.asesor][code].days++
+            }
+
+
+          }
+
+
         }else{
-          dataPrenom[item.asesor][item['exception']] = 1
+
+          tmp = item.rcode
+          code = item.rcode
+
+          if( item.exception == item.rcode ){
+            dataPrenom[item.asesor][item.rcode] = { days: 1, dates: [{ inicio: item.Fecha, fin: item.Fecha }] }
+          }else{
+
+            if( dataPrenom[item.asesor][item['exception']] ){
+              dataPrenom[item.asesor][item['exception']]['days']++
+            }else{
+              dataPrenom[item.asesor][item['exception']] = { days: 1, dates: [{ inicio: item.Fecha, fin: item.Fecha }] }
+            }
+
+            dataPrenom[item.asesor][item.rcode] = { days: 0, dates: [{ inicio: item.Fecha, fin: item.Fecha }] }
+          }
+
         }
 
       }else{
-        dataPrenom[item.asesor] = {
-          [item['exception']] : 1,
-          hx                  : item['hx']
+
+        tmp = item.rcode
+        code = item.rcode
+
+        let exc = {}
+
+
+        if( item.exception == item.rcode ){
+          exc[item.rcode] = { days: 1, dates: [{ inicio: item.Fecha, fin: item.Fecha }] }
+        }else{
+          exc[item.exception] = { days: 1, dates: [{ inicio: item.Fecha, fin: item.Fecha }] }
+          exc[item.rcode] = { days: 0, dates: [{ inicio: item.Fecha, fin: item.Fecha }] }
         }
+
+        dataPrenom[item.asesor] = exc
+        dataPrenom[item.asesor]['hx'] = item['hx']
       }
 
       if( dataShow[item.asesor] ){
@@ -251,7 +404,6 @@ export class PrenominaComponent implements OnInit {
 
     this.dataShow = dataShow
     this.dataPrenom = dataPrenom
-    console.log(this.dataPrenom)
 
     this.loading['building'] = false
     this.built = true
@@ -261,31 +413,58 @@ export class PrenominaComponent implements OnInit {
 
     let result = {
       code: '',
+      rcode: '',
       desc: ''
     }
 
-    if( this.dataAusentismos[data.asesor] ){
-      for( let aus in this.dataAusentismos[data.asesor] ){
-        if( moment(data.Fecha) >= moment(this.dataAusentismos[data.asesor][aus].Inicio) && moment(data.Fecha) <= moment(this.dataAusentismos[data.asesor][aus].Fin) ){
+    if( this.dataAusentismos[data.Fecha] && this.dataAusentismos[data.Fecha][data.asesor] ){
+
+      if( this.dataAusentismos[data.Fecha][data.asesor].a == 1 ){
+        result = {
+          code: this.dataAusentismos[data.Fecha][data.asesor]['Code'],
+          rcode: this.dataAusentismos[data.Fecha][data.asesor]['Code'],
+          desc: this.dataAusentismos[data.Fecha][data.asesor]['a_name']
+        }
+      }else{
+        if( this.dataAusentismos[data.Fecha][data.asesor].d == 1 ){
           result = {
-            code: this.dataAusentismos[data.asesor][aus]['Code'],
-            desc: this.dataAusentismos[data.asesor][aus]['Ausentismo']
+            code: 'D',
+            rcode: this.dataAusentismos[data.Fecha][data.asesor]['Code'],
+            desc: this.dataAusentismos[data.Fecha][data.asesor]['a_name']
           }
-          return result[type]
+        }else{
+          if( this.dataAusentismos[data.Fecha][data.asesor].b == 1 ){
+            result = {
+              code: 'B',
+              rcode: this.dataAusentismos[data.Fecha][data.asesor]['Code'],
+              desc: this.dataAusentismos[data.Fecha][data.asesor]['a_name']
+            }
+          }else{
+            result = {
+              code: 'NA',
+              rcode: this.dataAusentismos[data.Fecha][data.asesor]['Code'],
+              desc: this.dataAusentismos[data.Fecha][data.asesor]['a_name']
+            }
+          }
         }
       }
+
+
+      return result[type]
     }
 
     if( data.logs.j.in == null ){
       if( data.js == data.je ){
         result = {
           code: "D",
+          rcode: "D",
           desc: "Descanso"
         }
         return result[type]
       }else{
         result = {
           code: "FA",
+          rcode: "FA",
           desc: "Falta Injustificada"
         }
         return result[type]
@@ -294,6 +473,7 @@ export class PrenominaComponent implements OnInit {
       if( this.checkSA( data.js, data.je, data.logs.j.in, data.logs.j.out ) ){
         result = {
           code: "FA",
+          rcode: "FA",
           desc: "Salida Anticipada / Jornada < 60%"
         }
         return result[type]
@@ -301,11 +481,25 @@ export class PrenominaComponent implements OnInit {
         if( this.checkLength( data.js, data.je, data.logs.j.in, data.logs.j.out ) ){
           result = {
             code: "FA",
+            rcode: "FA",
             desc: "Jornada < 60%"
           }
           return result[type]
         }else{
-          return "A"
+          if( moment(data.Fecha).format('E') == 7 ){
+            result = {
+              code: "DOM",
+              rcode: "DOM",
+              desc: "Domingo Trabajado"
+            }
+          }else{
+            result = {
+              code: "A",
+              rcode: "A",
+              desc: "Asistencia"
+            }
+          }
+          return result
         }
       }
     }
@@ -479,4 +673,80 @@ export class PrenominaComponent implements OnInit {
 
   }
 
+  getCortes(){
+    this._api.restfulGet( '', 'Prenomina/listCortes' )
+            .subscribe( res => {
+
+              this.listCortesFlag = true
+              this.listCortes = res.data
+
+
+            }, err => {
+              console.log("ERROR", err)
+
+              this.listCortesFlag = false
+
+              let error = err.json()
+              this.toastr.error( error.msg, `Error ${err.status} - ${err.statusText}` )
+              console.error(err.statusText, error.msg)
+
+            })
+  }
+
+  selectedCorte( event ){
+    this.getSchedules( event )
+  }
+
+  printDates( item ){
+    let result = ''
+
+    for(let dates of item){
+
+      if( result != '' ){
+        result = `${result},`
+      }
+
+      if( dates.inicio == dates.fin ){
+        result = `${result} ${moment(dates.inicio).format('DD/MM')}`
+      }else{
+        result = `${result} ${moment(dates.inicio).format('DD/MM')} a ${moment(dates.fin).format('DD/MM')}`
+      }
+    }
+
+    return result
+  }
+
+  downloadXLS( id, title ){
+    this.toXls( id, title )
+
+  }
+
+  toXls( sheets, title ){
+
+    let wb = utils.table_to_book(document.getElementById(sheets), {raw: true});
+
+    for( let cell in wb.Sheets.Sheet1 ){
+
+      let match = cell.match(/^[A-Z]+[1]{1}$/g)
+      if( !match ){
+        if( cell.match(/^(?:(([A,B,J,N-R,T,V,X,Z]{1})|([A]{1}[B,D,F,H,J,L,N,P]{1})))(?=[1-9]{1}[0-9]*)/g) ){
+          if( wb.Sheets.Sheet1[cell].v != 'NA' ){
+            wb.Sheets.Sheet1[cell].t = 'n'
+            wb.Sheets.Sheet1[cell].v = wb.Sheets.Sheet1[cell].v.trim()
+          }
+        }
+      }
+    }
+
+    let wbout = write(wb, { bookType: 'xlsx', bookSST: true, type: 'binary' });
+
+    saveAs(new Blob([this.s2ab(wbout)], { type: 'application/octet-stream' }), `${title}.xlsx`)
+  }
+
+  s2ab(s) {
+    let buf = new ArrayBuffer(s.length);
+    let view = new Uint8Array(buf);
+    for (let i=0; i!=s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
+    return buf;
+  }
 }
